bc479df1993c3c9e40cc2fda31d39ceaf59d7f0b
Alex Borgman <info@webdesignborgman.nl>
2025-07-15 21:45:09 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="../public/apple-touch-icon.png" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Toaster richColors position="bottom-right" />
         <AuthProvider>
